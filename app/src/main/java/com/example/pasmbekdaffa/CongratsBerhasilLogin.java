405f0fd956f54336fe1050ac0e22d36132fbfca3
daffagantengbanget <daffarobanea@gmail.com>
2023-05-15 10:52:15 +0700

    dah selesai

---
+++ app/src/main/java/com/example/pasmbekdaffa/CongratsBerhasilLogin.java
@@ -0,0 +1,14 @@
+package com.example.pasmbekdaffa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CongratsBerhasilLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congrats_berhasil_login);
+    }
+}
