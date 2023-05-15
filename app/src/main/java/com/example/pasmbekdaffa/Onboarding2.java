17f4a046d7cff6686214e56947045871a9a1db57
daffagantengbanget <daffarobanea@gmail.com>
2023-05-15 07:51:34 +0700

    dah selesai

---
+++ app/src/main/java/com/example/pasmbekdaffa/Onboarding2.java
@@ -0,0 +1,28 @@
+package com.example.pasmbekdaffa;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Onboarding2 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding2, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
