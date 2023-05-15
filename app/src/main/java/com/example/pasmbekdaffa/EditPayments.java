b958f0b7f6f9bb88bcc9c13946d8b884cc80caae
daffagantengbanget <daffarobanea@gmail.com>
2023-05-16 05:20:27 +0700

    menambahkan beberapa tampilan

---
+++ app/src/main/java/com/example/pasmbekdaffa/EditPayments.java
@@ -0,0 +1,14 @@
+package com.example.pasmbekdaffa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditPayments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_payments);
+    }
+}
