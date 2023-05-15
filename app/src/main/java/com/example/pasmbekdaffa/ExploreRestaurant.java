c7acf9f66882946c3d7d92dcf507f7f2f7b48a34
daffagantengbanget <daffarobanea@gmail.com>
2023-05-16 05:15:33 +0700

    explore tamat

---
+++ app/src/main/java/com/example/pasmbekdaffa/ExploreRestaurant.java
@@ -0,0 +1,14 @@
+package com.example.pasmbekdaffa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExploreRestaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_restaurant);
+    }
+}
