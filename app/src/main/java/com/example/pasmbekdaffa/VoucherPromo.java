bf4a595c4483e21bac1a5258f11fd757213f98d7
daffagantengbanget <daffarobanea@gmail.com>
2023-05-16 09:16:46 +0700

    pokoknya selesai dah

---
+++ app/src/main/java/com/example/pasmbekdaffa/VoucherPromo.java
@@ -0,0 +1,14 @@
+package com.example.pasmbekdaffa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoucherPromo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voucher_promo);
+    }
+}
