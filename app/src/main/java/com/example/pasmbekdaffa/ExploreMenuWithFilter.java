4c6b3cfb88316b2a0be29ba1e1b05d5cded3b07f
daffagantengbanget <daffarobanea@gmail.com>
2023-05-16 07:44:27 +0700

    menambahkan beberapa file

---
+++ app/src/main/java/com/example/pasmbekdaffa/ExploreMenuWithFilter.java
@@ -0,0 +1,14 @@
+package com.example.pasmbekdaffa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExploreMenuWithFilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_menu_with_filter);
+    }
+}
---
+++ app/src/main/java/com/example/pasmbekdaffa/ExploreRestaurantWIthFilter.java
@@ -0,0 +1,14 @@
+package com.example.pasmbekdaffa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExploreRestaurantWIthFilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_restaurant_with_filter);
+    }
+}
