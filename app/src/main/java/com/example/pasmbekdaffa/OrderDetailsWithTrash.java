8a8ee8bd889517e8bbd69de5c0673c7cd00f1194
daffagantengbanget <daffarobanea@gmail.com>
2023-05-16 13:54:35 +0700

    menambahkan beberapa file

---
+++ app/src/main/java/com/example/pasmbekdaffa/OrderDetailsWithTrash.java
@@ -0,0 +1,14 @@
+package com.example.pasmbekdaffa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderDetailsWithTrash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details_with_trash);
+    }
+}
---
+++ app/src/main/java/com/example/pasmbekdaffa/YourOrders.java
@@ -0,0 +1,14 @@
+package com.example.pasmbekdaffa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YourOrders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_orders);
+    }
+}
